3f059043f01c64af273808c54654f158af5d3c84
Timothy Johnson <timothy.johnson@broadcom.com>
2020-03-24 10:00:25 -0400

    Fix jq not found
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- jenkins/bundleKeytar.sh
+++ jenkins/bundleKeytar.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-set -e
+set -ex
 
 mkdir prebuilds && cd prebuilds
 curl -fsL -o jq https://github.com/stedolan/jq/releases/latest/download/jq-linux64
 chmod +x ./jq
 
 curl -fs https://$1@api.github.com/repos/atom/node-keytar/releases/tags/v$2 |
-    jq -c '.assets[] | select (.name | contains("node"))' |
-    jq -r -c 'select (.browser_download_url) | .browser_download_url' |
+    ./jq -c '.assets[] | select (.name | contains("node"))' |
+    ./jq -r -c 'select (.browser_download_url) | .browser_download_url' |
     while IFS=$"\n" read -r c; do curl -sL -o `echo -n $(echo -n $c | md5sum | cut -c1-6)'-'$(basename $c)` $c; done
 
 rm ./jq
