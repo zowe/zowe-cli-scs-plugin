784b5056a055af42380011c7d3691be8102b17e1
Timothy Johnson <timothy.johnson@broadcom.com>
2020-10-20 10:23:41 -0400

    Update bundleKeytar script to make testing easier
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- jenkins/bundleKeytar.sh
+++ jenkins/bundleKeytar.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-set -ex
+# Usage: bash bundleKeytar.sh <keytarVersion> [githubAuthHeader]
 
+set -ex
+cd "$(git rev-parse --show-toplevel)"
+
+rm -rf prebuilds
 mkdir prebuilds && cd prebuilds
+
 curl -fsL -o jq https://github.com/stedolan/jq/releases/latest/download/jq-linux64
 chmod +x ./jq
 
-curl -fs https://$1@api.github.com/repos/atom/node-keytar/releases/tags/v$2 |
+curl -fs https://$2@api.github.com/repos/atom/node-keytar/releases/tags/v$1 |
     ./jq -c '.assets[] | select (.name | contains("node"))' |
-    ./jq -r -c 'select (.browser_download_url) | .browser_download_url' |
-    while IFS=$"\n" read -r c; do curl -fsL -o `echo -n $(echo -n $c | md5sum | cut -c1-6)'-'$(basename $c)` $c; done
+    ./jq -cr 'select (.browser_download_url) | .browser_download_url' |
+    while read -r bdu; do curl -fsL -o `echo -n $(echo -n $bdu | md5sum | cut -c1-6)'-'$(basename $bdu)` $bdu; done
 
 rm ./jq
 cd ..
