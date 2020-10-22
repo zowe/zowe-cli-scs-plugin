66e092ad8b859163818d68712ece5a8778513a2f
Timothy Johnson <timothy.johnson@broadcom.com>
2020-10-22 09:05:27 -0400

    Update Bash script based on PR suggestion
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- jenkins/bundleKeytar.sh
+++ jenkins/bundleKeytar.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # Usage: bash bundleKeytar.sh <keytarVersion> [githubAuthHeader]
-
 set -ex
-cd "$(git rev-parse --show-toplevel)"
 
+keytarVersion=$1
+githubAuthHeader=$2
+
+cd "$(git rev-parse --show-toplevel)"
 rm -rf prebuilds
 mkdir prebuilds && cd prebuilds
 
 curl -fsL -o jq https://github.com/stedolan/jq/releases/latest/download/jq-linux64
 chmod +x ./jq
 
-curl -fs https://$2@api.github.com/repos/atom/node-keytar/releases/tags/v$1 |
+curl -fs https://$githubAuthHeader@api.github.com/repos/atom/node-keytar/releases/tags/v$keytarVersion |
     ./jq -c '.assets[] | select (.name | contains("node"))' |
     ./jq -cr 'select (.browser_download_url) | .browser_download_url' |
     while read -r bdu; do curl -fsL -o `echo -n $(echo -n $bdu | md5sum | cut -c1-6)'-'$(basename $bdu)` $bdu; done
