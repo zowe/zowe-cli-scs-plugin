688d524c1dcd43af77bf1431f62d2f6279d0729a
Timothy Johnson <timothy.johnson@broadcom.com>
2021-03-02 12:58:01 -0500

    Add GitHub workflow to bundle prebuilds
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

---
+++ scripts/postpack.js
@@ -0,0 +1,29 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+const fs = require("fs");
+const glob = require("glob");
+const join = require("path").join;
+
+try {
+    // Rename TGZ to include version and platform info
+    const [oldFile] = glob.sync("*.tgz");
+    const packageJson = require(join(__dirname, "..", "package.json"));
+    const pkgName = packageJson.name.split("/")[1];
+    const pkgVersion = packageJson.version;
+    const nodeVersion = process.version.split(".")[0];
+    const platform = process.platform;
+    const arch = process.arch;
+    const newFile = `${pkgName}-v${pkgVersion}-node-${nodeVersion}-${platform}-${arch}.tgz`;
+    fs.renameSync(oldFile, newFile);
+    console.log(newFile);
+} catch (err) {
+    console.error(err);
+}
---
+++ scripts/prepack.js
@@ -0,0 +1,28 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+const fs = require("fs");
+const join = require("path").join;
+
+try {
+    // Add keytar to bundledDependencies in package.json
+    let packageJsonPath = join(__dirname, "..", "package.json");
+    let packageJson = require(packageJsonPath);
+    packageJson.bundledDependencies = Object.keys(packageJson.dependencies);
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
+    // Patch keytar package.json to remove custom install script
+    packageJsonPath = join(__dirname, "..", "node_modules", "keytar", "package.json");
+    packageJson = require(packageJsonPath);
+    delete packageJson.scripts.install;
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+} catch (err) {
+    console.error(err);
+}
